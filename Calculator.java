e3186322be24fa92ef47dc9bc6a580bf47f8a8a6
sumedhachowdary-coder <sumedhachallagundla2023@vitstudent.ac.in>
2026-01-07 14:27:29 +0530

    Added subtraction operation

--- Calculator.java
+++ Calculator.java
@@ -7,4 +7,9 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("Addition: " + add(10, 5));
     }
+    //collaboator 1
+    public static int subtract(int a, int b) {
+    return a - b;
+}
+
 }
